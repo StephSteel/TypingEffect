b779964589775692c1d90135b553d47ba01203c7
Steph Steel <138608345+StephSteel@users.noreply.github.com>
2023-10-16 20:10:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const dynamicText = document.querySelector("h1 span");
+
+const words = [
+    ". . . !",
+    "Funny!",
+    "Nice!",
+    "Reliable!",
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+
+
+    const currentChar = currentWord.substring(0, charIndex);
+
+
+    dynamicText.textContent = currentChar;
+
+
+    if (!isDeleting && charIndex < currentWord.length) {
+        charIndex++;
+
+
+        setTimeout(typeEffect, 200);
+    }
+    else if (isDeleting && charIndex > 0) {
+        charIndex--;
+
+
+        setTimeout(typeEffect, 100);
+    } else {
+        isDeleting = !isDeleting;
+
+        dynamicText.classList.remove("stop-blinking");
+
+
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+
+        setTimeout(typeEffect, 1200);
+    }
+}
+
+
+typeEffect();
